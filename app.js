7f077375611104be2f0e479d6aa31b6fd67b7d9a
esadakman <98649983+esadakman@users.noreply.github.com>
2022-06-02 23:44:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,31 @@
-const form = document.querySelector("#todo-form");
+// const form = document.querySelector("#todo-form");
 const inputTodo = document.querySelector("#todo");
-const addBtn = document.getElementById("addTodo");
-const search = document.querySelector("#search");
+// const addBtn = document.getElementById("addTodo");
+// const search = document.querySelector("#search");
 const todoList = document.querySelectorAll(".form-group")[1];
-const clearBtn = document.querySelector("#clear-todos");
+// const clearBtn = document.querySelector("#clear-todos");
 
-const firstCardBody = document.querySelectorAll(".card-body")[0];
-const secondCardBody = document.querySelectorAll(".card-body")[1];
-const items = document.querySelectorAll(".list-group li");
+// const firstCardBody = document.querySelectorAll(".card-body")[0];
+// const secondCardBody = document.querySelectorAll(".card-body")[1];
+// const items = document.querySelectorAll(".list-group li");
+document.querySelector(".delete-item");
+// let containerDiv = document.querySelector(".container");
 
-for (var i = 0; i > items.length; i++) {
-  tab.push(items[i].innerHTML);
-}
-
-for (var i = 0; i < items.length; i++) {
-  var tab = [],
-    index;
-
-  items[i].onclick = function () {
-    index = tab.indexOf(this.innerHTML);
-    console.log(this.innerHTML + " " + index);
-  };
-}
-
-addBtn.onclick = function () {
-  if (!inputTodo.value) {
-    alert("Lütfen bir dil giriniz");
-  } else {
+const containerDiv = document.querySelector(".container");
+containerDiv.addEventListener("click", (event) => {
+  if (event.target.classList.contains("addTodo")) {
+    console.log("add todo button clicked");
     addLI();
+  } else if (event.target.classList.contains("btn-danger")) {
+    console.log("clearrrrrrrrrrrrrr");
+  } else if (event.target.classList.contains("fa-remove")) {
+    console.log("deleteeee");
+  } else {
+    console.log("other elemnts clicked");
   }
-};
+});
+
 function addLI() {
-  // var inputText = document.createTextNode(inputTodo.value);
   const newElement = document.createElement("li");
   const link = document.createElement("a");
   // !============
@@ -46,8 +40,20 @@ function addLI() {
   inputTodo.value = "";
 }
 
-clearBtn.onclick = function () {
-  // todoList.innerHTML = "";
+// clearBtn.onclick = function () {
+//   // todoList.innerHTML = "";
+//   if (confirm("Tümünü silmek istediğinize emin misiniz ?")) {
+//     // Arayüzden todoları temizleme
+//     // todoList.innerHTML = ""; // Yavaş
+//     while (todoList.firstElementChild != null) {
+//       todoList.removeChild(todoList.firstElementChild);
+//     }
+//     localStorage.removeItem("todos");
+//   }
+// };
+
+function clearAll() {
+    let todosAll = document.querySelectorAll(".product-line-price")
   if (confirm("Tümünü silmek istediğinize emin misiniz ?")) {
     // Arayüzden todoları temizleme
     // todoList.innerHTML = ""; // Yavaş
@@ -56,19 +62,4 @@ clearBtn.onclick = function () {
     }
     localStorage.removeItem("todos");
   }
-};
-
-// addBtn.onclick = function () {
-//   if (!inputTodo.value) {
-//     alert("Lütfen bir dil giriniz");
-//   } else {
-//     let newElement = document.createElement("li");
-//     const link = document.createElement("a");
-//     newElement.className = "form-group-item d-flex justify-content-between";
-//     newElement.innerHTML += `${inputTodo.value}<a href="#" class="delete-item">
-//   <i class="fa fa-remove pe-3"></i>
-//   </a>`;
-//     todoList.appendChild(newElement);
-//     inputTodo.value = "";
-//   }
-// };
+}
