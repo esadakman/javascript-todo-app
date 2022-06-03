01905f10b1573bfebbd731f34f0b529f1a5a5fd1
esadakman <esadd26@gmail.com>
2022-06-04 00:31:36 +0300

    final changes

--- app.js
+++ app.js
@@ -92,7 +92,6 @@ function getTodosFromStorage() {
 
 function addTodoToStorage(newTodo) {
   let todos = getTodosFromStorage();
-
   todos.push(newTodo);
 
   localStorage.setItem("todos", JSON.stringify(todos));
